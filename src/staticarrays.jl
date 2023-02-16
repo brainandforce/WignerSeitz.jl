dbf494f58102cb00357f32ec508ebe70ebf49286
Brandon Flores <bsflores@wisc.edu>
2023-02-16 16:36:54 -0600

    Fixed construction of `SMatrix` lattice reduction type

--- src/staticarrays.jl
+++ src/staticarrays.jl
@@ -1,5 +1,6 @@
 using .StaticArrays
 
-function hermite_reduce(M::SMatrix{D1,D2,T}) where{D1,D2,T}
-    return SMatrix{D1,D2,T}(hermite_reduce!(convert(Matrix, M)))
+function hermite_reduce(M::SMatrix{D1,D2}) where{D1,D2}
+    L = hermite_reduce!(convert(Matrix, M))
+    return HermiteReducedLattice(SMatrix{D1,D2}(L.B), SMatrix{D2,D2}(getfield(L, :U)), L.info)
 end
