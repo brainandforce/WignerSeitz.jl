e0e3ac68c6cce59f01200787434ad3509f507ca4
Brandon Flores <bsflores@wisc.edu>
2023-02-16 22:41:39 -0600

    Added tools for superbase construction

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -19,3 +19,27 @@ projection(M::AbstractMatrix, a, b) = projection(M[:,a], M[:,b])
 Returns the Gram matrix of M.
 """
 gram(M::AbstractMatrix) = M' * M
+
+"""
+    WignerSeitz.superbase_vector(M::AbstractMatrix) -> AbstractVector
+
+Returns the superbase vector of basis `M`, the negative sum of all basis vector (columns) combined: 
+`-sum(M[:,n] for n in axes(M,2))`.
+"""
+superbase_vector(M::AbstractMatrix) = -sum(M[:,n] for n in axes(M,2))
+
+"""
+   WignerSeitz.superbase(M::AbstractMatrix) -> AbstractMatrix
+
+Constructs the superbase of `M`, which is equal to `M` with an additional column containing the
+negative sum of all basis vector (columns) combined: `-sum(M[:,n] for n in axes(M,2))`.
+"""
+superbase(M::AbstractMatrix) = hcat(M, superbase_vector(M))
+
+"""
+    WignerSeitz.selling(M::AbstractMatrix) -> typeof(M)
+
+Returns the Selling matrix of M, which is equal to the negative Gram matrix, but includes an extra
+column for the superbase vector.
+"""
+selling(M::AbstractMatrix) = -superbase(M)' * superbase(M)
